fc3ebd7de6c566af02fc0b5a15e87cfb9028a266
Jiwon Park <xoxojw.dev@gmail.com>
2023-06-11 15:37:22 +0900

    [Feat] 투두리스트 저장, 불러오기, 삭제 기능 추가

--- js/todo.js
+++ js/todo.js
@@ -1,13 +1,32 @@
 const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
-// => const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
+
+const TODOS_KEY = "todos";
+
+let toDos = [];
+
+const saveToDos = () => {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+};
+
+const deleteTodo = (event) => {
+  const li = event.target.parentElement;
+  li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos();
+};
 
 const paintToDo = (newTodo) => {
   const li = document.createElement("li");
+  li.id = newTodo.id;
   const span = document.createElement("span");
+  span.innerText = newTodo.text;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteTodo);
   li.appendChild(span);
-  span.innerText = newTodo;
+  li.appendChild(button);
   toDoList.appendChild(li);
 };
 
@@ -15,7 +34,21 @@ const handleToDoSubmit = (event) => {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
-  paintToDo(newTodo);
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj);
+  paintToDo(newTodoObj);
+  saveToDos();
 };
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
+};
