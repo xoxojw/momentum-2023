77af080e84dc71141184fd40dbda049aa9bcdb97
Jiwon Park <xoxojw.dev@gmail.com>
2023-06-20 18:07:29 +0900

    [chores] 이미지 경로 수정

--- js/background.js
+++ js/background.js
@@ -17,4 +17,4 @@ const images = [
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-document.body.style.backgroundImage = `url(../assets/${chosenImage})`
+document.body.style.backgroundImage = `url(assets/${chosenImage})`
